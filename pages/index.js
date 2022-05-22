21151ea493e0bfc58b4e96999349610bdbfbf980
Judith Herlambang <aditalmahdi@gmail.com>
2022-05-22 20:17:11 +0700

    please run

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,11 @@ export default function index() {
     <>
     
     <div className="container">
+
         <FirstRow />
         <SecondRow />
         <ThirdRow />
+        
     </div>
     
     </>
